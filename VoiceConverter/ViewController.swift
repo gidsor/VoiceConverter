41c1fb4a6d5bfa7f93e34685661bc31632e55f0a
Vadim Denisov <gidsor@gmail.com>
2018-09-04 23:10:35 +0500

    Added images

--- VoiceConverter/ViewController.swift
+++ VoiceConverter/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text = "Stopped"
+    }
+    
 }
 
