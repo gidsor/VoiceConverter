5a8f8dab773ed50798a60262911a53c785dc49f1
Vadim Denisov <gidsor@gmail.com>
2018-09-05 23:21:58 +0500

    Prepare for Segue and Receiving the Data

---
+++ VoiceConverter/PlaySoundsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySoundsViewController.swift
+//  VoiceConverter
+//
+//  Created by Vadim Denisov on 05.09.2018.
+//  Copyright © 2018 Vadim Denisov. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recorderAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
