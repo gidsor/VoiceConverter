5dbc8ae61d4a7caa9943232905b800159e19088e
Vadim Denisov <gidsor@gmail.com>
2018-09-06 21:14:21 +0500

    StackViews with Buttons for convert voice

--- VoiceConverter/PlaySoundsViewController.swift
+++ VoiceConverter/PlaySoundsViewController.swift
@@ -12,11 +12,27 @@ class PlaySoundsViewController: UIViewController {
 
     var recorderAudioURL: URL!
     
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        
     }
 }
